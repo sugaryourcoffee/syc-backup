3a1977e41d758e52097d4274f176e6a181787820
Pierre <pierre@sugaryourcoffee.de>
2013-01-24 08:52:45 +0100

    add test for compressing inexistent files

--- test/test_file_backup.rb
+++ test/test_file_backup.rb
@@ -7,7 +7,7 @@ class TestFileBackup < Test::Unit::TestCase
   context "Providing erroneous user input" do
 
     should "return error code 1 due to non existend file" do
-      fbu = Backup::FileBackup.new(["-f", "a,b,c"])
+      fbu = Backup::FileBackup.new(["f1"])
       begin
         backup_files = fbu.backup "test/backup"
       rescue SystemExit => e
@@ -16,11 +16,22 @@ class TestFileBackup < Test::Unit::TestCase
     end
 
     should "return error code 1 due to non existend file within list" do
-
+      fbu = Backup::FileBackup.new(["f1","f2","f3"])
+      begin
+        backup_files = fbu.backup "test/backup"
+      rescue SystemExit => e
+        assert_equal 1, e.status
+      end
     end
 
     should "return error code 2 due to non existend file to compress" do
-
+      fbu = Backup::FileBackup.new
+      begin
+        backup_files = fbu.compress ["a","b","c"]
+        flunk "expected error code 2"
+      rescue SystemExit => e
+        assert_equal 2, e.status
+      end
     end
   end
 
